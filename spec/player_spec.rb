ec9592db4e37b3861cde567a6b19fec44e9c6ccd
Nick Rathbone <nrathbone@joinhandshake.com>
2025-03-29 20:05:17 -0600

    add has_lost? method definition to player class and test logic to make sure it works

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,100 +10,81 @@ require './lib/deck'
 require './lib/player'
 #pry(main)> require './lib/player'
 #=> true
-
-RSpec describe Player do
+require 'pry'; binding.pry
+RSpec.describe Player do
 
   before(:each) do
+
     @card1 = Card.new(:diamond, 'Queen', 12)
-    # pry(main)> card1 = Card.new(:diamond, 'Queen', 12) 
-    # => #<Card:0x00000001073caea8 @rank=12, @suit=:diamond, @value="Queen">
-
     @card2 = Card.new(:spade, '3', 3)
-    # pry(main)> card2 = Card.new(:spade, '3', 3)
-    # => #<Card:0x00000001077cf030 @rank=3, @suit=:spade, @value="3">
-
     @card3 = Card.new(:heart, 'Ace', 14)
-    # pry(main)> Card.new(:heart, 'Ace', 14)
-    # => #<Card:0x00000001071d06c0 @rank=14, @suit=:heart, @value="Ace">
-        
+            
     @cards = [@card1, @card2, @card3]
-    # pry(main)> cards = [card1, card2, card3]
-    # =>[#<Card:0x00000001073caea8 @rank=12, @suit=:diamond, @value="Queen">,
-        #<Card:0x00000001077cf030 @rank=3, @suit=:spade, @value="3">,
-        #<Card:0x00000001071d06c0 @rank=14, @suit=:heart, @value="Ace">]
-
     @deck = Deck.new(@cards)
-    # pry(main)> deck = Deck.new(cards)
-    # => #<Deck:0x0000000108ff0010
-          #@cards=
-            #[#<Card:0x0000000107f65a38 @rank=12, @suit=:diamond, @value="Queen">,
-            #<Card:0x00000001089e0490 @rank=3, @suit=:spade, @value="3">,
-            #<Card:0x0000000108fd9d60 @rank=14, @suit=:heart, @value="Ace">]>
+    
+    @player = Player.new("Clarisa", @deck)
 
-
-          
-          
   end
 
+  # test to see if the player exists
   it "exists" do
     expect(@player).to be_an_instance_of(Player)
-
-    expect(Player.new).to eq(["Clarisa,"])
-    pry(main)> @player = Player.new('Clarisa', deck)
-    #=> #<Player:0x007f9cc3b4c988 @deck=#<Deck:0x007f9cc396bdf8 @cards=[#<Card:0x007f9cc3a73a98...>, #<Card:0x007f9cc3a03720...>, #<Card:0x007f9cc3a44c98...>]>, @name="Clarisa">
+    @player = Player.new("Clarisa", @deck)
+    #=> #<Player:0x0000000104404bb0, @deck= #<Deck:0x0000000104345a80, @cards= [#<Card:0x00000001040f53c0 @rank=12, @suit=:diamond, @value="Queen">,
+      #<Card:0x000000010409fc90 @rank=3, @suit=:spade, @value="3">,
+      #<Card:0x00000001042a0828 @rank=14, @suit=:heart, @value="Ace">]>, @name= "Clarisa">
   end
 
+  # test to see if the player has a name
+  it "has a name" do
+    expect(@player.name).to eq("Clarisa")
+    #pry(main)> @player.name
+    #=> "Clarisa"
+  end
 
+  # test to see if the player has a deck
+  it "has a deck" do
+    expect(@player.deck).to eq(@deck)
+    #pry(main)> @player.deck
+    #=> #<Deck:0x0000000104345a80, @cards= [#<Card:0x00000001040f53c0 @rank=12, @suit=:diamond, @value="Queen">,
+      #<Card:0x000000010409fc90 @rank=3, @suit=:spade, @value="3">,
+      #<Card:0x00000001042a0828 @rank=14, @suit=:heart, @value="Ace">]>
+  end
 
-
-
-
-
-
-
-
+  # test to see if the player has lost by having no more cards in the deck
+  it "deck empty and has lost" do
+    expect(@player.has_lost?).to eq(false)
+    #pry(main)> @player.has_lost?
+    #=> false
+  
+    expect(@player.deck.remove_card).to eq(@card1)
+    #pry(main)> @player.deck.remove_card
+    #=> #<Card:0x007f9cc3a73a98 @rank=12, @suit=:diamond, @value="Queen">
+    
+    expect(@player.has_lost?).to eq(false)
+    #pry(main)> @player.has_lost?
+    #=> false
+    
+    expect(@player.deck.remove_card).to eq(@card2)
+    #pry(main)> @player.deck.remove_card
+    #=> #<Card:0x007f9cc3a03720 @rank=3, @suit=:spade, @value="3">
+    
+    expect(@player.has_lost?).to eq(false)
+    #pry(main)> @player.has_lost?
+    #=> false
+    
+    expect(@player.deck.remove_card).to eq(@card3)
+    #pry(main)> @player.deck.remove_card
+    #=> #<Card:0x007f9cc3a44c98 @rank=14, @suit=:heart, @value="Ace">
+    
+    expect(@player.has_lost?).to eq(true)
+    #pry(main)> @player.has_lost?
+    #=> true
+    
+    expect(@player.deck.cards).to eq([])
+    #pry(main)> @player.deck
+    #=> #<Deck:0x007f9cc396bdf8 @cards=[]>
+  
+  end
 
 end
-
-
-
-
-
-
-
-
-
-
-
-pry(main)> player = Player.new('Clarisa', deck)
-
-
-pry(main)> player.name
-#=> "Clarisa"
-
-pry(main)> player.deck
-#=> #<Deck:0x007f9cc396bdf8 @cards=[#<Card:0x007f9cc3a73a98...>, #<Card:0x007f9cc3a03720...>, #<Card:0x007f9cc3a44c98...>]>
-
-pry(main)> player.has_lost?
-#=> false
-
-pry(main)> player.deck.remove_card
-#=> #<Card:0x007f9cc3a73a98 @rank=12, @suit=:diamond, @value="Queen">
-
-pry(main)> player.has_lost?
-#=> false
-
-pry(main)> player.deck.remove_card
-#=> #<Card:0x007f9cc3a03720 @rank=3, @suit=:spade, @value="3">
-
-pry(main)> player.has_lost?
-#=> false
-
-pry(main)> player.deck.remove_card
-#=> #<Card:0x007f9cc3a44c98 @rank=14, @suit=:heart, @value="Ace">
-
-pry(main)> player.has_lost?
-#=> true
-
-pry(main)> player.deck
-#=> #<Deck:0x007f9cc396bdf8 @cards=[]>
