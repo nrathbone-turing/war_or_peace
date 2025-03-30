ff549cbae3d4335aa937733ad965d228d243f877
Nick Rathbone <nrathbone@joinhandshake.com>
2025-03-29 21:13:53 -0600

    update comments on turn class and add more instance variable definitions to the turn spec file and test the globally applicable methods to create each player and their deck; begin to set up tests for each turn type's specific logic

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,42 +10,66 @@ class Turn
   end
 
   def type
+    # A :basic turn is one in which the rank_of_card_at(0) from the players’ decks are not the same rank.
     if @player1.deck.rank_of_card_at(0) != @player2.deck.rank_of_card_at(0)
       :basic
+    # A :war turn occurs when both players’ rank_of_card_at(0) are the same.
     elsif @player1.deck.rank_of_card_at(0) = @player2.deck.rank_of_card_at(0)
       :war
+    # :mutually_assured_destruction occurs when both players’ rank_of_card_at(0) AND rank_of_card_at(2) are the same.
     elsif @player1.deck.rank_of_card_at(0) = @player2.deck.rank_of_card_at(0) && @player1.deck.rank_of_card_at(2) = @player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
     end
   end
 
+  # this method will determine the winner of the turn.
   def winner
     # if the turn has a type of :basic, it will return whichever player has a higher rank_of_card_at(0)
-    if @turn.type == :basic
+    if @type == :basic
       if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
-        return @player1.name
+        return @player1
       else
-        return @player2.name
+        return @player2
       end
     # if the turn has a type of :war the winner will be whichever player has a higher rank_of_card_at(2)
-    elsif @turn.type == :war
+    elsif @type == :war
       if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
-        return @player1.name
+        return @player1
       else
-        return @player2.name
+        return @player2
       end
     # if the turn has a type of :mutually_assured_destruction the method will return No Winner
-    elsif @turn.type == :mutually_assured_destruction
+    elsif @turn == :mutually_assured_destruction
       return "No Winner"
     end
   end
-
+  
+  # when this method is called, cards will be sent from the players’ decks into the @spoils_of_war based on certain rules
   def pile_cards
-    if 
+    # for a :basic turn, each player will send one card (the top card) to the spoils pile
+    if @type == :basic
+      @player1.deck.remove_card[0]
+      @player2.deck.remove_card[0]
+      @spoils_of_war.push[@player1.deck.remove_card[0]]
+      @spoils_of_war.push[@player2.deck.remove_card[0]]
+    end
+    # for a :war turn, each player will send three cards (the top three cards) to the spoils pile
+    elsif @type == :war
+      
+    end
+    # for a :mutually_assured_destruction turn, each player will remove three cards from play (the top three cards in their deck). These cards are not sent to the spoils pile, they are simply removed from each players’ deck
+    elsif @type == :mutually_assured_destruction
+
+    end
   end
 
+  # this method will add each of the cards in the @spoils_of_war array to the winner of the turn.
   def award_spoils(@winner)
-
+    if @winner = @player1
+      deck1.push[@spoils_of_war]
+    elsif @winner = @player2
+      deck2.push[@spoils_of_war]
+    end
   end
 
 end
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -23,20 +23,123 @@ RSpec.describe Turn do
     @player1 = Player.new("Megan", @deck1)
     @player2 = Player.new("Aurora", @deck2)
 
+    @turn = Turn.new(@player1, @player2)
+
   end
 
   # test to see if player1 and player2 exist
-  if "player1 exists" do
+  it "player1 exists" do
     expect(@player1).to be_an_instance_of(Player)
+    #pry(main)> player1 = Player.new("Megan", deck1)    
+    #=> #<Player:0x007fa3edae29d0 @deck=#<Deck:0x007fa3eda472c8...>, @name="Megan">
   end
 
-  if "player2 exists" do
+  it "player2 exists" do
     expect(@player2).to be_an_instance_of(Player)
+    #pry(main)> @player2 = Player.new("Aurora", deck2)    
+    #=> #<Player:0x007fa3ed9e6568 @deck=#<Deck:0x007fa3ee11ee48...>, @name="Aurora">
+  end
+
+  # test to see if deck 1 and deck 2 exist
+  it "deck1 exists" do
+    expect(@deck1).to be_an_instance_of(Deck)
+    #pry(main)> @deck1 = Deck.new([card1, card2, card5, card8])    
+    #=> #<Deck:0x007fa3eda472c8 @cards=[#<Card:0x007fa3edaa0df0...>, #<Card:0x007fa3eda519a8...>,
+        #<Card:0x007fa3edb263d8...>, #<Card:0x007fa3eda89308...>]>
+  end
+
+  it "deck2 exists" do
+    expect(@deck2).to be_an_instance_of(Deck)
+    #pry(main)> @deck2 = Deck.new([card3, card4, card6, card7])    
+    #=> #<Deck:0x007fa3ee11ee48 @cards=[#<Card:0x007fa3ed98d9b8...>, #<Card:0x007fa3ee14ef80...>,
+        #<Card:0x007fa3eda3e1f0...>, #<Card:0x007fa3edad1cc0...>]>
+  end
+
+  # test to make sure a turn exists for each player
+  it "turn exists for player1" do
+    expect(@turn.player1).to eq([blah blah])
+    #pry(main)> @turn.player1
+    #=> #<Player:0x007fa3edae29d0 @deck=#<Deck:0x007fa3eda472c8...>, @name="Megan">
+  end
+
+  it "turn exists for player2" do
+    expect(@turn.player1).to eq([blah blah])
+    #pry(main)> @turn.player2
+    #=> #<Player:0x007fa3ed9e6568 @deck=#<Deck:0x007fa3ee11ee48...>, @name="Aurora">
+  end
+
+  end
+
+  # test logic for :basic turn
+  it ":basic turn type" do
+    expect(@turn.type).to eq(:basic)
+    #pry(main)> @turn.type
+    #=> :basic
+  
+    # test to make sure @spoils_of_war is empty
+    it "spoils_of_war is empty" do
+      expect(@turn.spoils_of_war).to eq([])
+      #pry(main)> @turn.spoils_of_war
+      # => []
+    end
+  
+  
+  
+  
+  
+  
+  
   end
 
   
+  
+  # test logic for :war turn
+  it ":war turn type" do
+    expect(@turn.type).to eq(:war)
+    #pry(main)> @turn.type
+    #=> :war
+  
+    # test to make sure @spoils_of_war is not empty
+    it "spoils_of_war is empty" do
+      expect(@turn.spoils_of_war).to eq([blah blah])
+      #pry(main)> @turn.spoils_of_war
+      # => [blah blah]
+    end
+  
+  
+  
+  
+  
+  
+  
+  end
 
-
+  
+  
+  
+  # test logic for :mutually_assured_destruction turn
+  it ":mutually_assured_destruction turn type" do
+    expect(@turn.type).to eq(:mutually_assured_destruction)
+    #pry(main)> @turn.type
+    #=> :mutually_assured_destruction
+  
+    # test to make sure @spoils_of_war is empty
+    it "spoils_of_war is empty" do
+      expect(@turn.spoils_of_war).to eq([])
+      #pry(main)> @turn.spoils_of_war
+      # => []
+    end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  end
 
 
 
