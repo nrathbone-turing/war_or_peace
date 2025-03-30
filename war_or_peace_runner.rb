8ca8e811590dedd3a9e8b0bd9d41fd1cb1fdfb7f
Nick Rathbone <nrathbone@joinhandshake.com>
2025-03-30 13:51:34 -0600

    Create runner file for project and add text to print to console upon startup

---
+++ war_or_peace_runner.rb
@@ -0,0 +1,6 @@
+# upon startup, display welcome message, introduce players, and provide user input to start playing
+puts "Welcome to War! (or Peace) This game will be played with 52 cards."
+puts "The players today are Megan and Aurora."
+puts "Type 'GO' to start the game!"
+puts "------------------------------------------------------------------"
+
