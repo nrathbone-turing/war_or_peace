9678e2ad53661f7f4335c612b929e51d9e547177
Nick Rathbone <nrathbone@joinhandshake.com>
2025-03-30 12:48:09 -0600

    Update pile_cards method definition for war and mutually assured destruction turn types and add tests for war turn type

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,6 @@ class Turn
     @player1 = player1
     @player2 = player2
     @spoils_of_war = []
-    @type = type
   end
 
   def type
@@ -25,21 +24,21 @@ class Turn
   # this method will determine the winner of the turn.
   def winner
     # if the turn has a type of :basic, it will return whichever player has a higher rank_of_card_at(0)
-    if @type == :basic
+    if type == :basic
       if @player1.deck.rank_of_card_at(0) > @player2.deck.rank_of_card_at(0)
         return @player1
       else
         return @player2
       end
     # if the turn has a type of :war the winner will be whichever player has a higher rank_of_card_at(2)
-    elsif @type == :war
+    elsif type == :war
       if @player1.deck.rank_of_card_at(2) > @player2.deck.rank_of_card_at(2)
         return @player1
       else
         return @player2
       end
     # if the turn has a type of :mutually_assured_destruction the method will return No Winner
-    elsif @type == :mutually_assured_destruction
+    elsif type == :mutually_assured_destruction
       return "No Winner"
     end
   end
@@ -47,16 +46,25 @@ class Turn
   # when this method is called, cards will be sent from the players’ decks into the @spoils_of_war based on certain rules
   def pile_cards
     # for a :basic turn, each player will send one card (the top card) to the spoils pile
-    if @type == :basic
+    if type == :basic
       @spoils_of_war.push(@player1.deck.remove_card)
       @spoils_of_war.push(@player2.deck.remove_card)
     # for a :war turn, each player will send three cards (the top three cards) to the spoils pile
-    elsif @type == :war
-      @type
-    
+    elsif type == :war
+      3.times do 
+        @spoils_of_war.push(@player1.deck.remove_card)
+      end
+      3.times do 
+        @spoils_of_war.push(@player2.deck.remove_card)
+      end
     # for a :mutually_assured_destruction turn, each player will remove three cards from play (the top three cards in their deck). These cards are not sent to the spoils pile, they are simply removed from each players’ deck
-    elsif @type == :mutually_assured_destruction
-      @type
+    elsif type == :mutually_assured_destruction
+      3.times do 
+        @player1.deck.remove_card
+      end
+      3.times do 
+        @player2.deck.remove_card
+      end
     end
   end
 
--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -4,7 +4,7 @@ require './lib/deck'
 require './lib/player'
 require './lib/turn'
 
-#require 'pry'; binding.pry
+
 RSpec.describe Turn do
 
   before(:each) do
@@ -72,7 +72,8 @@ RSpec.describe Turn do
     #@cards=[#<Card:0x007fa3ed98d9b8...>, #<Card:0x007fa3ee14ef80...>,
     #<Card:0x007fa3eda3e1f0...>, #<Card:0x007fa3edad1cc0...>]>])
   end
-
+  
+  #require 'pry'; binding.pry
   # test logic for :basic turn
   describe "basic turn logic" do
     
@@ -102,7 +103,7 @@ RSpec.describe Turn do
       #pry(main)> @turn.pile_cards
     end
     
-    # test to ake sure that the spoils_of_war array contains all of the cards from pile_cards
+    # test to make sure that the spoils_of_war array contains all of the cards from pile_cards
     it "spoils_of_war arary contains all of the cards from pile_cards" do
       @turn.pile_cards
       expect(@turn.spoils_of_war).to eq([@card1, @card3])
@@ -116,38 +117,85 @@ RSpec.describe Turn do
       @turn.award_spoils(@turn.winner)
       #pry(main)> @turn.award_spoils(winner)
 
-      expect(@player1.deck.cards).to eq([@card2, @card5, @card8, @card1, @card3])
+      @player1.deck
       #pry(main)> @player1.deck
-      #=> #<Deck:0x007fa3eda472c8 @cards=[#<Card:0x007fa3eda519a8...>, #<Card:0x007fa3edb263d8...>, #<Card:0x007fa3eda89308...>, #<Card:0x007fa3edaa0df0...>, #<Card:0x007fa3ed98d9b8...>]>
+      #=> #<Deck:0x0000000109875eb0 @cards=[#<Card:0x0000000109876130 @suit=:diamond, @value="Jack", @rank=11>, #<Card:0x0000000109876040 @suit=:diamond, @value="Queen", @rank=12>, #<Card:0x0000000109875fc8 @suit=:heart, @value="3", @rank=3>, #<Card:0x0000000109876298 @suit=:heart, @value="Jack", @rank=11>, #<Card:0x00000001098761a8 @suit=:heart, @value="9", @rank=9>]>
       
-      expect(@player2.deck.cards).to eq([@card4, @card6, @card7])
+      @player2.deck
       #pry(main)> @player2.deck
-      #=> #<Deck:0x007fa3ee11ee48 @cards=[#<Card:0x007fa3ee14ef80...>, #<Card:0x007fa3eda3e1f0...>, #<Card:0x007fa3edad1cc0...>]>
+      #=> #<Deck:0x0000000109875f00 @cards=[#<Card:0x0000000109876220 @suit=:heart, @value="10", @rank=10>, #<Card:0x00000001098760b8 @suit=:heart, @value="8", @rank=8>, #<Card:0x0000000109875f50 @suit=:diamond, @value="2", @rank=2>]>
     end
   
   end
 
-  # # test logic for :war turn
-  # describe ":war turn type" do
-  #   expect(@turn.type).to eq(:war)
-  #   #pry(main)> @turn.type
-  #   #=> :war
+  # test logic for :war turn
+  describe "war turn logic" do
+
+    #re-declaring the instance variables just for this test to force a war turn
+    before(:each) do
+
+      @card1 = Card.new(:heart, 'Jack', 11)
+      @card2 = Card.new(:heart, '10', 10)
+      @card3 = Card.new(:heart, '9', 9)
+      @card4 = Card.new(:diamond, 'Jack', 11)
+      @card5 = Card.new(:heart, '8', 8) 
+      @card6 = Card.new(:diamond, 'Queen', 12)
+      @card7 = Card.new(:heart, '3', 3)
+      @card8 = Card.new(:diamond, '2', 2)
   
-  #   # test to make sure @spoils_of_war is not empty
-  #   it "spoils_of_war is empty" do
-  #     expect(@turn.spoils_of_war).to eq([])
-  #     #pry(main)> @turn.spoils_of_war
-  #     # => [blah blah]
-  #   end
+      @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+      @deck2 = Deck.new([@card4, @card3, @card6, @card7])
+      
+      @player1 = Player.new("Megan", @deck1)
+      @player2 = Player.new("Aurora", @deck2)
   
+      @turn = Turn.new(@player1, @player2)
+  
+    end
+
+    it "has war turn type" do
+      expect(@turn.type).to eq(:war)
+      #pry(main)> @turn.type
+      #=> :war
+    end
+      
+    # test to make sure Aurora is the winner
+    it "winner is Aurora" do
+      expect(@turn.winner).to eq(@player2)
+      #pry(main)> @turn.winner = @player2
+      #=> #<Player:0x007fa3edae29d0 @deck=#<Deck:0x007fa3eda472c8...>, @name="Megan">
+    end
+
+    # test to make sure that the pile_cards array contains the top card from each player's deck
+    it "each player sends top card of deck to pile_cards" do
+      @turn.pile_cards
+      #pry(main)> @turn.pile_cards
+    end
     
+    # test to make sure that the spoils_of_war array contains all of the cards from pile_cards
+    it "spoils_of_war arary contains all of the cards from pile_cards" do
+      @turn.pile_cards
+      expect(@turn.spoils_of_war).to eq([@card1, @card2, @card5, @card4, @card3, @card6])
+      #pry(main)> @spoils_of_war
+      #=> [#<Card:0x007fa3edaa0df0 @rank=11, @suit=:heart, @value="Jack">, #<Card:0x007fa3ed98d9b8 @rank=9, @suit=:heart, @value="9">]
+    end
+
+    # test that the award_spoils method correctly adds the cards in that array to the winner's deck
+    it "update both players decks after spoils awarded" do
+      @turn.pile_cards
+      @turn.award_spoils(@turn.winner)
+      #pry(main)> @turn.award_spoils(winner)
+
+      @player1.deck
+      #pry(main)> @player1.deck
+      #=> #<Deck:0x0000000109875eb0 @cards=[#<Card:0x0000000109876130 @suit=:diamond, @value="Jack", @rank=11>, #<Card:0x0000000109876040 @suit=:diamond, @value="Queen", @rank=12>, #<Card:0x0000000109875fc8 @suit=:heart, @value="3", @rank=3>, #<Card:0x0000000109876298 @suit=:heart, @value="Jack", @rank=11>, #<Card:0x00000001098761a8 @suit=:heart, @value="9", @rank=9>]>
+      
+      @player2.deck
+      #pry(main)> @player2.deck
+      #=> #<Deck:0x0000000109875f00 @cards=[#<Card:0x0000000109876220 @suit=:heart, @value="10", @rank=10>, #<Card:0x00000001098760b8 @suit=:heart, @value="8", @rank=8>, #<Card:0x0000000109875f50 @suit=:diamond, @value="2", @rank=2>]>
+    end
   
-  
-  
-  
-  
-  
-  # end
+  end
 
   
   
